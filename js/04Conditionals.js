8a1ce85f77eefe971869577218b738508be5b44c
unknown <mariano12martinez@gmail.com>
2025-11-22 17:10:51 -0400

    condicionales en js

---
+++ js/04Conditionals.js
@@ -0,0 +1,12 @@
+// Condicionales en JavaScript
+
+// Evaluacion de edad para adulto mayor
+const edad = 65;
+
+if (edad >= 65) {
+    console.log("Es adulto mayor");
+} else if (edad >= 18) {
+    console.log("Es mayor de edad");
+} else {
+    console.log("No es adulto mayor");
+}
