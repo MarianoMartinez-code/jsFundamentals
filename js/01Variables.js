2478054bcb72728048c9dbd891444f02af25363e
unknown <mariano12martinez@gmail.com>
2025-11-22 16:23:54 -0400

    first commit

---
+++ js/01Variables.js
@@ -0,0 +1,15 @@
+// Declarar variables
+const nombre = "Juan";
+let edad = 25;
+let apellido = "Perez";
+let genero = "Masculino";
+let isMayorDeEdad = true;
+let precio = 19.99;
+
+// Mostrar variables
+alert("Nombre: " + nombre);
+alert("Edad: " + edad);
+alert("Apellido: " + apellido);
+alert("Genero: " + genero);
+alert("Mayor de edad: " + isMayorDeEdad);
+alert("Precio: " + precio);
