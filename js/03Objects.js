cc7da03831763d6d460392963272e7048b385c4f
unknown <mariano12martinez@gmail.com>
2025-11-22 16:55:34 -0400

    objects in js

---
+++ js/03Objects.js
@@ -0,0 +1,17 @@
+// Declarar objetos en JavaScript
+const persona = {
+    nombre: "Juan",
+    edad: 25,
+    genero: "Masculino",
+    isMayorDeEdad: true,
+    precio: 19.99,
+    hobbies: ["Deportes", "Musica", "Cine"],
+    direccion: {
+        calle: "Calle 1",
+        numero: 123,
+        ciudad: "Ciudad 1"
+    }
+};
+
+console.log(persona);
+
