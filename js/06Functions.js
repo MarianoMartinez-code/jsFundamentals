c652d52d1ae7f1080ae0d125a3f9e71e14d44218
unknown <mariano12martinez@gmail.com>
2025-11-22 17:32:35 -0400

    functions in js

---
+++ js/06Functions.js
@@ -0,0 +1,21 @@
+// Funciones en JavaScript
+
+// Declarar funciones
+function suma(a, b) {
+    return a + b;
+}
+
+// Llamar funciones
+console.log(suma(1, 2));
+
+// Funciones flecha
+const resta = (a, b) => a - b;
+console.log(resta(2, 1));
+
+// Funciones anonimas o expresivas
+const multiplicacion = function(a, b) {
+    return a * b;
+}
+console.log(multiplicacion(2, 3));
+
+
