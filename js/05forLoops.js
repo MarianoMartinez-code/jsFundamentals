a8f275e46b495fe659fa9c91ec2e922e0caedcd7
unknown <mariano12martinez@gmail.com>
2025-11-22 17:23:18 -0400

    for loops in js

---
+++ js/05forLoops.js
@@ -0,0 +1,12 @@
+// Bucles en JavaScript
+
+// Bucle for
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// Recorrer array
+const nombres = ["Juan", "Maria", "Pedro", "Ana", "Luis"];
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+}
